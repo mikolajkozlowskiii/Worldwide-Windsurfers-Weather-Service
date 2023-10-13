3c47f71111cff21328e2ba4ca6d4c1cdc52ba945
mikolajkozlowskiii <mikolajkozlowskiii@gmail.com>
2023-10-13 12:55:03 +0200

    Initialize Spring Boot project

---
+++ src/main/java/com/example/windsurfers/WindsurfersApplication.java
@@ -0,0 +1,13 @@
+package com.example.windsurfers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindsurfersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WindsurfersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/windsurfers/WindsurfersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.windsurfers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindsurfersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
